68a2ae4b08474d8804b989b008204440811bea18
praneethlearns <praneethkavi2006@gmail.com>
2026-06-01 12:04:27 +0200

    IMage capute GUI

--- source_code/vision/calibration/run_capture_images.py
+++ source_code/vision/calibration/run_capture_images.py
@@ -29,7 +29,7 @@ Example:
 """
 # Run camera calibration after this
 
-from source_code.utility.paths import RAW_IMAGES_DIR, ensure_directories
+from source_code.utility.paths import *
 import cv2
 
 ensure_directories()
@@ -57,7 +57,7 @@ while True:
         print(f"Saved: {filename}")
         count += 1
     if key == ord('\r'):   #if enter key is pressed, save the image
-        filename = RAW_IMAGES_DIR / f"Img.png"
+        filename = OBJ_SEGMENTATION_DIR / f"Img.png"
         cv2.imwrite(str(filename), frame)
         print(f"Saved: {filename}")
         count += 1
